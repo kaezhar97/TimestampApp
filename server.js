06fe2f40a9f9bbe15f4614e2e399429b5d1678a8
Glitch (wonderful-nest) <none>
2018-12-28 23:01:31 +0000

    🌆💕 Checkpoint ./server.js:1615799/7

--- server.js
+++ server.js
@@ -18,15 +18,13 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
-
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
