ac3dbbc359b403999882d1b2db33b1ee4d537e49
Glitch (wonderful-nest) <none>
2019-01-04 04:00:32 +0000

    🤒👹 Checkpoint ./server.js:1615799/7

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.get("/api/timestamp/", function(req, res){
 app.get("/api/timestamp/:date_string", function (req, res) {
   var miliseconds = parseInt(req.params.date_string);
   if (miliseconds != NaN) {
-    var date = new Date();
+    var date = new Date(miliseconds);
   }
   else {
     var date = new Date(req.params.date_string);
